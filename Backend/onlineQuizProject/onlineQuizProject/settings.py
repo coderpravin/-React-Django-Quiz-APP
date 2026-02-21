8aa118a3a11bd8e09d83073db63d1981ae92bbb9
coderpravin <impravin00@gmail.com>
2026-02-21 13:40:19 +0530

    Add dj-database-url

--- Backend/onlineQuizProject/onlineQuizProject/settings.py
+++ Backend/onlineQuizProject/onlineQuizProject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,6 +93,7 @@ DATABASES = {
 }
 '''
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -101,7 +104,13 @@ DATABASES = {
         'PORT': 5432,   
     }
 }
+'''
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
