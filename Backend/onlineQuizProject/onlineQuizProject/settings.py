786702e857838ce5bf2fd4abe97f4af0eed8ef42
coderpravin <impravin00@gmail.com>
2026-02-21 10:53:26 +0530

    Setup PostgreSQL database and add quiz fixture data

--- Backend/onlineQuizProject/onlineQuizProject/settings.py
+++ Backend/onlineQuizProject/onlineQuizProject/settings.py
@@ -78,10 +78,23 @@ WSGI_APPLICATION = 'onlineQuizProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'onlinequiz',
+        'USER': 'postgres',
+        'PASSWORD': 'pramodmore',
+        'HOST': 'localhost', 
+        'PORT': 5432,   
     }
 }
 
